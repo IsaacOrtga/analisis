3359cfb8aa4b103a830e9777246e1f0b3d00d8db
IsaacOrtga <ortga.isaac@gmail.com>
2022-04-24 19:06:05 +0200

    Ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,9 @@
+
+
+function operacion(){
+    let valor1 = prompt("Número");
+    let valor2 = prompt("Número");
+    alert((parseInt(valor1, 0)) + ((parseInt(valor2, 0))));
+    alert((parseInt(valor1, 0)) - ((parseInt(valor2, 0))));
+
+}
